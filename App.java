802c390332c48324d34effd9dca132982a2bdb88
LuisDev01Lab <luisqrja@gmail.com>
2024-06-25 20:28:11 -0500

    feat: First steps in Java

---
+++ App.java
@@ -0,0 +1,29 @@
+public class App {
+  public static void main(String[] args) {
+    String userName = "Aldair";
+    int userAge = 22;
+    // Varaible types
+    double temperature = 32.5;
+    double priceProduct = 12.5;
+    String fullName = "Luis Aldair Quispe Rios";
+    String birthDate = "2021-09-01";
+    int inhabitants = 1000;
+    int filmDurationMinutes = 120;
+    boolean isProductAvailable = true;
+    double distanceKilometers = 12.5;
+    String phone = "123456789";
+    int productQuantity = 10;
+
+    System.out.println("Hello, my name is " + userName + " and I am " + userAge + " years old.");
+    System.out.println("The temperature is " + temperature + "°C.");
+    System.out.println("The price of the product is " + priceProduct + " $.");
+    System.out.println("My full name is " + fullName + ".");
+    System.out.println("My birth date is " + birthDate + ".");
+    System.out.println("The number of inhabitants is " + inhabitants + ".");
+    System.out.println("The film duration is " + filmDurationMinutes + " minutes.");
+    System.out.println("The product is available: " + isProductAvailable + ".");
+    System.out.println("The distance is " + distanceKilometers + " km.");
+    System.out.println("My phone number is " + phone + ".");
+    System.out.println("The quantity of the product is " + productQuantity + ".");
+  }
+}
